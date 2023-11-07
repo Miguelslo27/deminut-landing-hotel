50838333c5157b85aefcf6fe137dbd9250053eef
Miguel <miguelmail2006@gmail.com>
2023-11-06 22:58:41 -0300

    fix/going-live - Update screenshots and phone number

--- src/components/Landing/Hero/Hero.js
+++ src/components/Landing/Hero/Hero.js
@@ -64,7 +64,7 @@ const HeroSection = () => {
                 <a
                   className="btn sApp-btn text-uppercase"
                   target="_blank"
-                  href="https://wa.me/59891066416"
+                  href="https://wa.me/59892073609"
                 >
                   <FontAwesomeIcon
                     icon={faWhatsapp}

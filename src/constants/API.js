9da24b2d994eb2125e07f42d31df85b74af963f5
Miguel <miguelmail2006@gmail.com>
2023-11-07 22:35:01 -0300

    fix/going-live - Fix api host and contact form

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,9 +1,9 @@
 const getApiUrl = () => {
   console.log(`NODE_ENV`, process.env.NODE_ENV);
-
   if (process.env.NODE_ENV === 'development') {
     return `http://localhost:3000/v1`;
   }
-  return `https://api.staging.deminut.com/v1`;
+  return `https://api-stg.deminut.com/v1`;
 }
+
 export const API_URL = getApiUrl();
